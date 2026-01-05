58fc551d5383e9fea14cc0235a9725ba29ed1999
Durga prasad <plakshmi8080@gmail.com>
2026-01-05 13:07:13 +0530

    solved errors

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -59,7 +59,7 @@ const SelectScrollDownButton = React.forwardRef<
     )}
     {...props}
   >
-    <ChevronDown className="h-4 w-4" />
+    <HiChevronDown className="h-4 w-4" />
   </SelectPrimitive.ScrollDownButton>
 ))
 SelectScrollDownButton.displayName =
